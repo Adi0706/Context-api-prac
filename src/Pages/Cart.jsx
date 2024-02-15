b8e1a1add4548b5df66d92ebd289abf4049378cb
Aditya Bhattacharjee <adityab76@gmail.com>
2024-02-15 14:49:16 +0530

    updated code

--- src/Pages/Cart.jsx
+++ src/Pages/Cart.jsx
@@ -14,8 +14,8 @@ function Cart() {
   return (
     <>
       <Navbar />
-      <div className='flex justify-center'>
-        <div className='border border-solid border-black'>
+      <div className='flex items-center justify-around'>
+        <div className='border border-solid border-black mt-12'>
           {/*
             Use Set to get unique product IDs and map over them
             to display product information
@@ -26,9 +26,12 @@ function Cart() {
             // Return the product information
             return (
               <div key={productId} className='flex flex-row p-4'>
-                <img src={product.cardImage} alt={`product-image-${productId}`} />
+                <img src={product.cardImage} alt={`product-image-${productId}`} className='w-52 h-52'/>
+                <div className='border border-solid border-black flex flex-col items-center w-auto h-auto justify-center'>
                 <p className="text-lg font-bold">{product.cardName}</p>
                 <p className="text-xl">{product.cardPrice}</p>
+                </div>
+               
               </div>
             );
           })}
