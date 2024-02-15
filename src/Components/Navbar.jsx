2725d2f83ce86ffee7fa2ece32919c05d4461b9f
Aditya Bhattacharjee <adityab76@gmail.com>
2024-02-15 13:50:13 +0530

    cart page setup done

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -1,22 +1,22 @@
-import React from 'react'
+import React from 'react';
 import { FaShoppingCart } from "react-icons/fa";
-import {Link} from 'react-router-dom'
-import {useMyCartContext} from '../Context/CartContext'
-function Navbar() {
+import { Link } from 'react-router-dom';
+import { useMyCartContext } from '../Context/CartContext';
 
-  const {cart} = useMyCartContext();
+function Navbar() {
+  const { cart } = useMyCartContext();
+
   return (
-    <>
-          <nav className=' mt-0 bg-emerald-400 text-white '>
-            <ul className='list-none flex items-center justify-evenly p-5'>
-               <Link to='/'><li className='text-3xl'>CONTEXT-API CART SYSTEM</li></Link> 
-               <Link to='/cart'><li className=''><FaShoppingCart className='w-7 h-7'/>({cart.length})</li></Link> 
-             
-            </ul>
-            
-          </nav>
-    </>
+    <nav className=' mt-0 bg-emerald-400 text-white'>
+      <ul className='list-none flex items-center justify-evenly p-5'>
+        <Link to='/'><li className='text-3xl'>CONTEXT-API CART SYSTEM</li></Link>
+        <Link to='/cart'>
+          
+          <li className=''><FaShoppingCart className='w-7 h-7' />({cart?.length || 0})</li>
+        </Link>
+      </ul>
+    </nav>
   )
 }
 
-export default Navbar
+export default Navbar;
--- src/Pages/Cart.jsx
+++ src/Pages/Cart.jsx
@@ -1,13 +1,44 @@
-import React from 'react'
-import Navbar from '../Components/Navbar'
+import React from 'react';
+import Navbar from '../Components/Navbar';
+import { useMyCartContext } from '../Context/CartContext';
 
 function Cart() {
+  const { cart } = useMyCartContext();
+
+  // Check if cart is not an array
+  if (!Array.isArray(cart)) {
+    console.error("Cart is not an array!");
+    return null; // Return early if cart is not an array
+  }
+
   return (
-  <>
-  <Navbar/>
-  
-  </>
-  )
+    <>
+      <Navbar />
+      <div className='flex justify-center'>
+        <div className='border border-solid border-black'>
+          {/*
+            Use Set to get unique product IDs and map over them
+            to display product information
+          */}
+          {[...new Set(cart.map((product) => product.id))].map((productId) => {
+            // Find the first matching product by its id
+            const product = cart.find((item) => item.id === productId);
+            // Return the product information
+            return (
+              <div key={productId} className='flex flex-row p-4'>
+                <img src={product.cardImage} alt={`product-image-${productId}`} />
+                <p className="text-lg font-bold">{product.cardName}</p>
+                <p className="text-xl">{product.cardPrice}</p>
+              </div>
+            );
+          })}
+        </div>
+        <div className='border border-solid border-black'>
+          hello
+        </div>
+      </div>
+    </>
+  );
 }
 
-export default Cart
+export default Cart;
